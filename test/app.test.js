9d1efbe642f395e8b5001d33a69a85853de48ede
Pipo-hub <petersantiagorommikael@gmail.com>
2026-07-21 23:27:23 +0800

    Initial commit with sample project

---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('API Tests', () => {
+  test('GET / should return hello message', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Hello World!');
+  });
+
+  test('GET /api/users should return users array', async () => {
+    const response = await request(app).get('/api/users');
+    expect(response.statusCode).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0]).toHaveProperty('name');
+  });
+});
