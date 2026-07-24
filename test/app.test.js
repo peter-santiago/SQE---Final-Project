f51be8e004631bba5a386faf2b07c2df93692c64
Pipo-hub <petersantiagorommikael@gmail.com>
2026-07-24 08:26:46 +0800

    test: intentionally break a test 2

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('API Tests', () => {
   test('GET / should return hello message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-     // Change this to make it fail:
+    // This should FAIL pag hinde meron nanamang tao mawawala sa mundo today
     expect(response.body).toHaveProperty('message', 'Wrong Message!');
   });
 
