f4352ff25d6db73559a198d0d4a981bcc9b3ab36
Pipo-hub <petersantiagorommikael@gmail.com>
2026-07-24 08:30:45 +0800

    test: add unused variable for linting

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Add to break lint
+const unusedVariable = "This will cause a lint error";
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
